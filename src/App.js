9a9013ab9c843f68b31cf0a9c6fa93192d85f449
purwar1997 <shubhampurwar511@gmail.com>
2024-11-24 21:33:46 +0530

    feat: setup routing to access different pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,106 @@
-export default function App() {
-  return (
-    <main>
-      <h1 className='text-3xl font-semibold text-center'>Hello World</h1>
-    </main>
-  );
-}
+import { lazy, Suspense } from 'react';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+
+import AppLayout from './components/layout/AppLayout';
+import LoadingSpinner from './components/ui/LoadingSpinner';
+import Protected from './components/Protected';
+
+import LoginPage from './pages/LoginPage';
+import NotFoundPage from './pages/NotFoundPage';
+import ErrorPage from './pages/ErrorPage';
+
+const UserListPage = lazy(() => import('./pages/UserListPage'));
+const AddUserPage = lazy(() => import('./pages/AddUserPage'));
+const EditUserPage = lazy(() => import('./pages/EditUserPage'));
+const RoleListPage = lazy(() => import('./pages/RoleListPage'));
+const AddRolePage = lazy(() => import('./pages/AddRolePage'));
+const EditRolePage = lazy(() => import('./pages/EditRolePage'));
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path='/login' element={<LoginPage />} />
+
+      <Route path='/' element={<AppLayout />} errorElement={<ErrorPage />}>
+        <Route
+          index
+          element={
+            <Protected>
+              <Suspense fallback={<LoadingSpinner />}>
+                <UserListPage />
+              </Suspense>
+            </Protected>
+          }
+        />
+
+        <Route
+          path='users/add'
+          element={
+            <Protected>
+              <Suspense fallback={<LoadingSpinner />}>
+                <AddUserPage />
+              </Suspense>
+            </Protected>
+          }
+        />
+
+        <Route
+          path='users/:id/edit'
+          element={
+            <Protected>
+              <Suspense fallback={<LoadingSpinner />}>
+                <EditUserPage />
+              </Suspense>
+            </Protected>
+          }
+        />
+
+        <Route
+          path='roles'
+          element={
+            <Protected>
+              <Suspense fallback={<LoadingSpinner />}>
+                <RoleListPage />
+              </Suspense>
+            </Protected>
+          }
+        />
+
+        <Route
+          path='roles/add'
+          element={
+            <Protected>
+              <Suspense fallback={<LoadingSpinner />}>
+                <AddRolePage />
+              </Suspense>
+            </Protected>
+          }
+        />
+
+        <Route
+          path='roles/:id/edit'
+          element={
+            <Protected>
+              <Suspense fallback={<LoadingSpinner />}>
+                <EditRolePage />
+              </Suspense>
+            </Protected>
+          }
+        />
+      </Route>
+
+      <Route path='*' element={<NotFoundPage />} />
+    </Route>
+  )
+);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { fetchLoggedInUserAsync } from './app/slices/authSlice';
+import store from './app/store';
 import App from './App';
 import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+(async () => {
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+
+  try {
+    const userId = localStorage.getItem('loggedInUserId');
+
+    if (userId) {
+      await store.dispatch(fetchLoggedInUserAsync(userId)).unwrap();
+    }
+  } catch (error) {
+    console.log(error);
+  }
+
+  root.render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+})();
