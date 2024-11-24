1f4c9f1373b9c138ba79e835803c031ee815a125
purwar1997 <shubhampurwar511@gmail.com>
2024-11-25 04:10:09 +0530

    Rename environment variable

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const client = axios.create({
-  baseURL: process.env.REACT_APP_SERVER_URL + '/api',
+  baseURL: process.env.REACT_APP_API_URL + '/api',
 });
 
 client.defaults.headers.post['Content-Type'] = 'application/json';
