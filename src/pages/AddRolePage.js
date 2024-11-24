0fa6db547b386a0f96599d75b0417fc641fc1dd7
purwar1997 <shubhampurwar511@gmail.com>
2024-11-24 21:47:23 +0530

    feat: allow creation of new users and roles

---
+++ src/pages/AddRolePage.js
@@ -0,0 +1,105 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { useShowPermissions } from '../hooks';
+import { addNewRoleAsync } from '../app/slices/roleSlice';
+import { classNames } from '../utils/helperFunctions';
+import ButtonLoader from '../components/ui/ButtonLoader';
+import PermissionDisplayModal from '../components/PermissionDisplayModal';
+import { PERMISSIONS, PERMISSIONS_DESCRIPTION } from '../constants';
+
+const AddRolePage = () => {
+  const [roleTitle, setRoleTitle] = useState('');
+  const [rolePermissions, setRolePermissions] = useState([]);
+  const [openPermissionModal, setOpenPermissionModal] = useState(false);
+  const [addStatus, setAddStatus] = useState('idle');
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const togglePermissionModal = () => setOpenPermissionModal(!openPermissionModal);
+  useShowPermissions(togglePermissionModal);
+
+  const updateTitle = e => setRoleTitle(e.target.value);
+
+  const updatePermissions = e => {
+    if (e.target.checked) {
+      setRolePermissions(rolePermissions.concat(e.target.value));
+    } else {
+      setRolePermissions(rolePermissions.filter(value => value !== e.target.value));
+    }
+  };
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+
+    try {
+      setAddStatus('pending');
+      await dispatch(addNewRoleAsync({ title: roleTitle, permissions: rolePermissions })).unwrap();
+      navigate('/roles');
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setAddStatus('idle');
+    }
+  };
+
+  return (
+    <main className='page-height px-12 py-10 flex flex-col items-center gap-10'>
+      <h1 className='text-3xl'>Add new role</h1>
+
+      <form className='max-w-2xl w-full space-y-5' onSubmit={handleSubmit}>
+        <div className='flex-1 flex flex-col gap-2'>
+          <label className='font-medium text-gray-500' htmlFor='title'>
+            Title
+          </label>
+
+          <input
+            className='w-full px-3 py-2 rounded-md'
+            type='text'
+            name='title'
+            id='title'
+            value={roleTitle}
+            onChange={updateTitle}
+            required
+          />
+        </div>
+
+        <div>
+          <p>Select permissions for the role:</p>
+
+          <div>
+            {Object.values(PERMISSIONS).map(permission => (
+              <div className='flex gap-1' key={permission}>
+                <input
+                  type='checkbox'
+                  name='permissions'
+                  id={permission}
+                  value={permission}
+                  onChange={updatePermissions}
+                  required
+                />
+                <label htmlFor={permission}>{PERMISSIONS_DESCRIPTION[permission]}</label>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <button
+          className={classNames(
+            'w-full h-12 bg-indigo-600 text-white font-medium rounded-md hover:bg-indigo-700 flex justify-center items-center',
+            addStatus === 'pending' ? 'cursor-wait' : ''
+          )}
+          type='submit'
+          disabled={addStatus === 'pending'}
+        >
+          {addStatus === 'pending' ? <ButtonLoader /> : 'Add Role'}
+        </button>
+      </form>
+
+      {openPermissionModal && <PermissionDisplayModal closeModal={openPermissionModal} />}
+    </main>
+  );
+};
+
+export default AddRolePage;
---
+++ src/pages/AddUserPage.js
@@ -0,0 +1,104 @@
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { useShowPermissions } from '../hooks';
+import { fetchRolesAsync, selectRoles } from '../app/slices/roleSlice';
+import { addNewUserAsync } from '../app/slices/userSlice';
+import { classNames } from '../utils/helperFunctions';
+import ButtonLoader from '../components/ui/ButtonLoader';
+import PermissionDisplayModal from '../components/PermissionDisplayModal';
+
+const AddUserPage = () => {
+  const [userDetails, setUserDetails] = useState({ name: '', role: '' });
+  const [openPermissionModal, setOpenPermissionModal] = useState(false);
+  const [addStatus, setAddStatus] = useState('idle');
+
+  const roles = useSelector(selectRoles);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const togglePermissionModal = () => setOpenPermissionModal(!openPermissionModal);
+
+  useEffect(() => {
+    dispatch(fetchRolesAsync());
+  }, [dispatch]);
+
+  useShowPermissions(togglePermissionModal);
+
+  const handleChange = e => {
+    setUserDetails({ ...userDetails, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+
+    try {
+      setAddStatus('pending');
+      await dispatch(addNewUserAsync(userDetails)).unwrap();
+      navigate('/');
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setAddStatus('idle');
+    }
+  };
+
+  return (
+    <main className='page-height px-12 py-10 flex flex-col items-center gap-10'>
+      <h1 className='text-3xl'>Add new user</h1>
+
+      <form className='max-w-2xl w-full space-y-5' onSubmit={handleSubmit}>
+        <div className='flex-1 flex flex-col gap-2'>
+          <label className='font-medium text-gray-500' htmlFor='name'>
+            Username
+          </label>
+
+          <input
+            className='w-full px-3 py-2 rounded-md'
+            type='text'
+            name='name'
+            id='name'
+            value={userDetails.name}
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+        <div>
+          <p>Choose the role to be assigned:</p>
+
+          <div>
+            {roles.map(role => (
+              <div className='flex gap-1' key={role.id}>
+                <input
+                  type='radio'
+                  name='role'
+                  id={role.title}
+                  value={role.id}
+                  onChange={handleChange}
+                  required
+                />
+                <label htmlFor={role.title}>{role.title}</label>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <button
+          className={classNames(
+            'w-full h-12 bg-indigo-600 text-white font-medium rounded-md hover:bg-indigo-700 flex justify-center items-center',
+            addStatus === 'pending' ? 'cursor-wait' : ''
+          )}
+          type='submit'
+          disabled={addStatus === 'pending'}
+        >
+          {addStatus === 'pending' ? <ButtonLoader /> : 'Add User'}
+        </button>
+      </form>
+
+      {openPermissionModal && <PermissionDisplayModal closeModal={openPermissionModal} />}
+    </main>
+  );
+};
+
+export default AddUserPage;
