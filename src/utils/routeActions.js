590a1cc02ec6e1ba478f7d0ab51ea662626cb085
purwar1997 <shubhampurwar511@gmail.com>
2024-11-27 06:47:26 +0530

    chore: remove unnecessary computed properties

--- src/utils/routeActions.js
+++ src/utils/routeActions.js
@@ -3,10 +3,10 @@ import { PERMISSIONS } from '../constants';
 const { VIEW_USERS, ADD_USER, EDIT_USER, VIEW_ROLES, ADD_ROLE, EDIT_ROLE } = PERMISSIONS;
 
 export const getRouteActions = id => ({
-  ['/']: VIEW_USERS,
-  ['/users/add']: ADD_USER,
+  '/': VIEW_USERS,
+  '/users/add': ADD_USER,
   [`/users/${id}/edit`]: EDIT_USER,
-  ['/roles']: VIEW_ROLES,
-  ['/roles/add']: ADD_ROLE,
+  '/roles': VIEW_ROLES,
+  '/roles/add': ADD_ROLE,
   [`/roles/${id}/edit`]: EDIT_ROLE,
 });
